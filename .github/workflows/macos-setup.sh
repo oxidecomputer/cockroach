96e07a031ed0fc44c030419bab2e1303b8ffdabf
iliana etaoin <iliana@oxide.computer>
2024-11-22 05:28:55 +0000

    remove workaround

--- .github/workflows/macos-setup.sh
+++ .github/workflows/macos-setup.sh
@@ -14,12 +14,4 @@ tar xf "node-v$NODE_VERSION-darwin-arm64.tar.xz"
 mv "yarn-$YARN_VERSION.js" "node-v$NODE_VERSION-darwin-arm64/bin/yarn"
 chmod a+x "node-v$NODE_VERSION-darwin-arm64/bin/yarn"
 export PATH="$PWD/go/bin:$PWD/node-v$NODE_VERSION-darwin-arm64/bin:$PATH"
-
-# Apply patch to fix golang/go#53000
-pushd go/src
-patch -p2 <"$SOURCE_DIR/.github/workflows/e66f895667cd51d0d28c42d369a803c12db8bb35.patch"
-go build cmd/cgo
-popd
-mv go/src/cgo go/pkg/tool/darwin_arm64/cgo
-
 popd
