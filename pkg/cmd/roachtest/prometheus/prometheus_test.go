a9fc834e1cd29dcc31a029346b01f4f459cef9a6
iliana etaoin <iliana@oxide.computer>
2025-04-17 18:50:50 +0000

    correctly fix roachtest/prometheus test

--- pkg/cmd/roachtest/prometheus/prometheus_test.go
+++ pkg/cmd/roachtest/prometheus/prometheus_test.go
@@ -17,7 +17,6 @@ import (
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/option"
 	logger "github.com/cockroachdb/cockroach/pkg/roachprod/logger"
-	"github.com/cockroachdb/cockroach/pkg/testutils/skip"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
@@ -35,8 +34,6 @@ func nilLogger() *logger.Logger {
 }
 
 func TestMakeYAMLConfig(t *testing.T) {
-	skip.IgnoreLint(t, "broken on go1.22")
-
 	ctx := context.Background()
 	testCases := []struct {
 		desc string
@@ -51,13 +48,13 @@ func TestMakeYAMLConfig(t *testing.T) {
 			mockCluster: func(ctrl *gomock.Controller) Cluster {
 				c := NewMockCluster(ctrl)
 				c.EXPECT().
-					ExternalIP(ctx, nilLogger(), []int{1}).
+					ExternalIP(ctx, gomock.Any(), []int{1}).
 					Return([]string{"127.0.0.1"}, nil)
 				c.EXPECT().
-					ExternalIP(ctx, nilLogger(), []int{3, 4, 5}).
+					ExternalIP(ctx, gomock.Any(), []int{3, 4, 5}).
 					Return([]string{"127.0.0.3", "127.0.0.4", "127.0.0.5"}, nil)
 				c.EXPECT().
-					ExternalIP(ctx, nilLogger(), []int{6}).
+					ExternalIP(ctx, gomock.Any(), []int{6}).
 					Return([]string{"127.0.0.6"}, nil)
 				return c
 			},
@@ -111,13 +108,13 @@ scrape_configs:
 			mockCluster: func(ctrl *gomock.Controller) Cluster {
 				c := NewMockCluster(ctrl)
 				c.EXPECT().
-					ExternalIP(ctx, nilLogger(), []int{3, 4, 5}).
+					ExternalIP(ctx, gomock.Any(), []int{3, 4, 5}).
 					Return([]string{"127.0.0.3", "127.0.0.4", "127.0.0.5"}, nil)
 				c.EXPECT().
-					ExternalIP(ctx, nilLogger(), []int{6}).
+					ExternalIP(ctx, gomock.Any(), []int{6}).
 					Return([]string{"127.0.0.6"}, nil)
 				c.EXPECT().
-					ExternalIP(ctx, nilLogger(), []int{8, 9}).
+					ExternalIP(ctx, gomock.Any(), []int{8, 9}).
 					Return([]string{"127.0.0.8", "127.0.0.9"}, nil)
 				return c
 			},
